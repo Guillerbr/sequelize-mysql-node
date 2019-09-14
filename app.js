55ca0513044dd4f238b5e2c4dc017b5b3a9b2a36
Guillerbr <ghminvest@gmail.com>
2019-09-14 13:52:07 -0300

    express x-powered disable

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.disable('x-powered-by');
+
 
 app.use(logger('dev'));
 app.use(express.json());
